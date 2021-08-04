5759232a5d704caa6049fbcfb2329c0027569c6a
Kimia Haj <Hajkimia@outlook.com>
2021-08-04 15:42:37 +0430

    Create Entities, Identity, and  Mapping - Build Database andconnection string

---
+++ NewsWebsite.Entities/Comment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NewsWebsite.Entities
+{
+   public class Comment
+    {
+        [Key]
+        public string CommentId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Descriotion { get; set; }
+        public string NewsId { get; set; }
+        public bool IsConfirm { get; set; }
+        public DateTime? PostageDateTime { get; set; }
+
+        public string ParentCommentId { get; set; }
+        public virtual Comment comment { get; set; }
+        public virtual News News { get; set; }
+
+
+
+    }
+}
--- NewsWebsite/Startup.cs
+++ NewsWebsite/Startup.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NewsWebsite.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +13,22 @@ using System.Threading.Tasks;
 
 namespace NewsWebsite
 {
+    
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+        
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<NewsDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SqlServer")));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
